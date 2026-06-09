b0948378a98055bd543c29de7fc97d9aea328949
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2026-06-09 12:11:18 +0530

    feat: add asynchronous JavaScript examples with promises and setTimeout

---
+++ advancedjs/practice.js
@@ -0,0 +1,32 @@
+// console.log("script")
+// setTimeout(()=>console.log(1),0)
+// Promise.resolve().then(()=>console.log(2))
+// console.log("end")
+
+// console.log("script")
+// setTimeout(()=>console.log(1),0)
+// Promise.resolve()
+// .then(()=>console.log(2))
+// .then(()=>console.log(3))
+// .then(()=>console.log(4))
+// console.log("end")
+
+// console.log("begins")
+// setTimeout(()=>{
+//     console.log("uppersetTimeout1")
+//      Promise.resolve()
+//     .then(()=>console.log("Promise1"))
+// },0)
+
+// new Promise(function(resolve,reject){
+//     console.log("promise 2")
+//     setTimeout(function(){
+//         console.log("set timeout 2")
+//         resolve("resolve 1")
+//     },0)
+// }).then((res)=>{
+//     console.log("dot then 1");
+//     setTimeout(()=>{
+//         console.log("setTimeout 1")
+//     },0)
+// })
