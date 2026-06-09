f6ca201d8e9157f51ed08434670039658719107e
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2026-06-09 14:58:22 +0530

    feat: add initial three.js example demonstrating variable hoisting and function declarations

---
+++ advancedjs/three.js
@@ -0,0 +1,25 @@
+// console.log(1)
+// setTimeout(()=>console.log(2),0)
+// setTimeout(()=>console.log(3),0)
+
+// const p=Promise.resolve()
+// for(let i=0;i<2;i++){
+//     p.then(()=>{setTimeout(()=>{
+//         console.log(4)
+//         setTimeout(()=>console.log(5),0)
+//         p.then(()=>console.log(6))
+//     },0)
+// })
+// }
+
+// console.log(8)
+
+console.log(a)
+
+function a(){}
+
+console.log(a)
+
+var a=3
+
+console.log(a)
